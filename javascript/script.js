33d0749fc37b0b2aa4b72bebd5a7d5f137004b77
Flackooo <rovicvillanueva15@gmail.com>
2024-05-25 15:02:25 +0800

    first commit

--- javascript/script.js
+++ javascript/script.js
@@ -36,6 +36,42 @@ function slider(){
     setTimeout('slider()',3000);
 };
 
+
+// Single Product Images
+let MainImg = document.getElementById("MainImg");
+let smallimg = document.getElementsByClassName("small-img");
+
+smallimg[0].onclick = function (){
+    MainImg.src = smallimg[0].src;
+};
+smallimg[1].onclick = function (){
+    MainImg.src = smallimg[1].src;
+};
+smallimg[1].onclick = function (){
+    MainImg.src = smallimg[1].src;
+};
+smallimg[2].onclick = function (){
+    MainImg.src = smallimg[2].src;
+};
+smallimg[3].onclick = function (){
+    MainImg.src = smallimg[3].src;
+};
+
+// Window location
+function win(){
+    window.location.href='../SingleProduct/Kproduct.html';
+}
+
+
+
+
+
+
+
+
+
+
+
 // Navbar @media queries
 // $(document).ready(function(){
 //     $('.font-logo').on('click',function(){
